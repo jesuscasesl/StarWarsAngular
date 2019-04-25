2d061d83fd54f0bfcb37200ed1d6d7e01108c346
Jesus Manuel Castillo Eslava <jesus.castillo.eslava@gmail.com>
2019-04-26 00:32:47 +0200

    create service

---
+++ src/app/services/starwars.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StarwarsService {
+
+  constructor( private http: HttpClient ) { }
+
+    getQuery( queryEnd: string ) {
+      const url = `https://swapi.co/api/${ queryEnd }`;
+      return this.http.get( url );
+    }
+
+    getPeoples() {
+      return this.getQuery('people');
+    }
+
+    getFilms() {
+      return this.getQuery('films');
+    }
+}
